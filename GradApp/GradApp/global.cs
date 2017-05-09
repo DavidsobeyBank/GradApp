36c2a726f7ff2238b67b9ae016520e2c8ea67fc9
DavidsobeyBank <david.sobey@standardbank.co.za>
2017-05-09 11:37:43 +0200

    Added Identity

---
+++ GradApp/GradApp/global.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Claims;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+
+namespace GradApp
+{
+    public class global
+    {
+        //AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
+    }
+}
