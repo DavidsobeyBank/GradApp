3c9929494a8893d3b775dfe670583b8ab922fb9c
DavidsobeyBank <david.sobey@standardbank.co.za>
2017-05-10 16:00:05 +0200

    ...

--- Tables/dbo.Graduate.sql
+++ Tables/dbo.Graduate.sql
@@ -3,7 +3,7 @@ CREATE TABLE [dbo].[Graduate]
 [GraduateID] [int] NOT NULL IDENTITY(1, 1),
 [Name] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [Surname] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[Email] [nchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Email] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [PreferredRole] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [Degree] [varchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [GraduateYear] [int] NULL
