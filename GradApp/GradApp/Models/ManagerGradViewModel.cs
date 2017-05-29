a213d50b2e16ddf0efa121c9ba03b11e1567bf92
Sobey <David.Sobey@standardbank.co.za>
2017-05-29 14:45:31 +0200

    test

---
+++ GradApp/GradApp/Models/ManagerGradViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GradApp.Models
+{
+    public class ManagerGradViewModel
+    {
+        public Rotation Rotation { get; set; }
+        public Project Project { get; set; }
+        public ManagerGradViewModel(Rotation rotation, Project project)
+        { 
+            Rotation = rotation;
+            Project = project;
+        }
+    }
+}
